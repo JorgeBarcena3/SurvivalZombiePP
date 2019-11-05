0ab59577fa7aae4715079ada10b6c69e833cf178
Jorge Barcena Lumbreras <j.barcenalumbreras@gmail.com>
2019-11-05 12:26:19 +0100

    Rotation and xbox controller fixed

--- Assets/Scripts/Player/PlayerControls.cs
+++ Assets/Scripts/Player/PlayerControls.cs
@@ -2,29 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public static class ControllerAxis
+{
+    public static string LHorizontal = "Horizontal";
+    public static string LVertical = "Vertical";
+    public static string RHorizontal = "Mouse X";
+    public static string RVertical = "Mouse Y";
+
+
+}
+
 public class PlayerControls : MonoBehaviour
 {
-    // Start is called before the first frame update
+    /// <summary>
+    /// Velocidad del jugador
+    /// </summary>
     public float speed;
+
+    /// <summary>
+    /// Rigidbody component
+    /// </summary>
     private Rigidbody rb;
+
+    /// <summary>
+    /// Componente animator
+    /// </summary>
+    private Animator anim;
+
+
+
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        
+        anim = GetComponent<Animator>();
+        speed *= 100;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //controles de movimiento con mando
-        rb.AddForce(Vector3.left * Input.GetAxis("Left Stick Horizontal 1") * speed * Time.deltaTime, ForceMode.Acceleration);
-        rb.AddForce(Vector3.forward * Input.GetAxis("Left Stick Vertical 1") * speed * Time.deltaTime, ForceMode.Acceleration);
+        //Controles de movimiento con mando
+        rb.AddForce(-Vector3.right * -Input.GetAxis(ControllerAxis.LHorizontal) * speed * Time.deltaTime, ForceMode.Acceleration);
+        rb.AddForce(Vector3.forward * -Input.GetAxis(ControllerAxis.LVertical) * speed * Time.deltaTime, ForceMode.Acceleration);
 
-        
+        float angle = Mathf.Atan2(Input.GetAxis(ControllerAxis.RVertical), Input.GetAxis(ControllerAxis.RHorizontal)) * Mathf.Rad2Deg;
+        rb.rotation = Quaternion.Euler(new Vector3(0, angle, 0));
 
-        //controles de movimiento con teclado
+
+        //Controles de movimiento con teclado -- Solo Debug
         if (Input.GetKey(KeyCode.W))
-        { rb.AddForce(Vector3.forward* speed * Time.deltaTime,ForceMode.Impulse); }
+        { rb.AddForce(Vector3.forward * speed * Time.deltaTime, ForceMode.Impulse); }
         if (Input.GetKey(KeyCode.S))
         { rb.AddForce(-Vector3.forward * speed * Time.deltaTime, ForceMode.Impulse); }
         if (Input.GetKey(KeyCode.A))
@@ -33,5 +61,21 @@ public class PlayerControls : MonoBehaviour
         { rb.AddForce(-Vector3.left * speed * Time.deltaTime, ForceMode.Impulse); }
 
 
+    }
+
+    /// <summary>
+    /// Update cuando se han cargado todas las fisicas
+    /// </summary>
+    void FixedUpdate()
+    {
+        //  Cambiamos la animacion segun la velocidad
+        if (Input.GetAxis(ControllerAxis.LHorizontal) == 0 && Input.GetAxis(ControllerAxis.LVertical) == 0)
+        {
+            anim.SetBool("Moving", false);
+        }
+        else
+        {
+            anim.SetBool("Moving", true);
+        }
     }
 }
---
+++ Assets/Scripts/Player/ZombieFollower.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieFollower : MonoBehaviour
+{
+
+    /// <summary>
+    /// Rigidbody del zombie
+    /// </summary>
+    private Rigidbody rb;
+
+    /// <summary>
+    /// Position to follow
+    /// </summary>
+    public GameObject targetPosition;
+
+    /// <summary>
+    /// Velocidad del zombie
+    /// </summary>
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        speed *= 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 direction = (targetPosition.transform.position - this.transform.position).normalized;
+        rb.AddForce(direction *  speed * Time.deltaTime, ForceMode.Acceleration);
+
+        transform.LookAt(targetPosition.transform);
+
+
+
+    }
+}
