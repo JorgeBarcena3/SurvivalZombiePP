b4d0f840f5a791ca56b86b30fbd5b0eca76f7cbb
pablojimenodev <pablojimenodev@gmail.com>
2019-11-18 19:44:41 +0100

    NavMeshAgent con el zombie

--- Assets/Scripts/Player/ZombieFollower.cs
+++ Assets/Scripts/Player/ZombieFollower.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.AI;
 using UnityEngine;
 
 public class ZombieFollower : MonoBehaviour
@@ -20,24 +21,32 @@ public class ZombieFollower : MonoBehaviour
     /// </summary>
     public float speed;
 
+    /// <summary>
+    /// Componente que controla el movimiento del zombie
+    /// </summary>
+    NavMeshAgent agent;
+
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        //rb = GetComponent<Rigidbody>();
+        //speed *= 100;
 
-        speed *= 100;
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = targetPosition.transform.position;  // Posición que persigue el NavMeshAgent
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        Vector3 direction = (targetPosition.transform.position - this.transform.position).normalized;
-        rb.AddForce(direction *  speed * Time.deltaTime, ForceMode.Acceleration);
+        //Vector3 direction = (targetPosition.transform.position - this.transform.position).normalized;
+        //rb.AddForce(direction *  speed * Time.deltaTime, ForceMode.Acceleration);
+
+        // Movimiento con el NavMeshAgent 
+        agent.destination = targetPosition.transform.position;
 
         transform.LookAt(targetPosition.transform);
-
-
 
     }
 }
