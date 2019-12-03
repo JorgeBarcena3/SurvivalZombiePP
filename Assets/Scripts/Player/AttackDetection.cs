1012a5340dc82ebd96192520c16f5ed799d4e063
miguelangelgil <miguel.gilmartin12@gmail.com>
2019-12-03 16:44:16 +0100

    empezando ataque de los zombies, todabia no acabado

---
+++ Assets/Scripts/Player/AttackDetection.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackDetection : MonoBehaviour
+{
+    public bool CanAttack = false;
+    private ZombieFollower myZombie;
+    void Start()
+    {
+        myZombie = GetComponentInParent<ZombieFollower>();
+        
+    }
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "player") 
+        {
+            CanAttack = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "player")
+        {
+            CanAttack = false;
+        }
+
+    }
+    private void Attack() 
+    {
+
+    }
+}
+ 
